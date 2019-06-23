e37ab2eff40f1122a096c5d78093f7d006cf032d
Atheeq <atheeqrc@gmail.com>
2019-06-23 17:41:13 +0530

    Final AF

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// import App from './App';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import  Courseweb from './components/common/Courseweb'
+import  CommonHome from './components/common/CommonHome'
+import AdminLogin from "./components/admin/AdminLogin";
+import InstructorLogin from "./components/instructor/InstructorLogin";
+import StudentLogin from "./components/student/StudentLogin";
+
+
+ReactDOM.render(<Courseweb />, document.getElementById('root'));
+
+if(localStorage.getItem('userType') == 'admin'){
+    ReactDOM.render(<AdminLogin />, document.getElementById('root'));
+}
+if(localStorage.getItem('userType') == 'instructor'){
+    ReactDOM.render(<InstructorLogin />, document.getElementById('root'));
+}
+
+if(localStorage.getItem('userType') == 'student'){
+    ReactDOM.render(<StudentLogin />, document.getElementById('root'));
+}
+
+
